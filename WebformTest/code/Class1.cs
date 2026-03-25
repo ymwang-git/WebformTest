eb4a3e889f04f73d86c10117ea0266d1ae84a30e
min <min@test.com>
2026-03-25 09:47:48 +0800

    新增檔案

---
+++ WebformTest/code/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebformTest.code
+{
+    public class Class1
+    {
+        boot sc = true;
+
+    }
+}
